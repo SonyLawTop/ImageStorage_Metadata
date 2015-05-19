2bc9fc4e2b6b63446b4907794d0a57ee3571a184
SonyLawTop <topsonylaw@gmail.com>
2015-05-19 21:29:01 +0700

    c

--- ImageStorage_Metadata/app/src/main/java/com/sonylawtop/root/imagestorage_metadata/MainActivity.java
+++ ImageStorage_Metadata/app/src/main/java/com/sonylawtop/root/imagestorage_metadata/MainActivity.java
@@ -1,29 +1,26 @@
 package com.sonylawtop.root.imagestorage_metadata;
 
+import java.io.FileNotFoundException;
 import android.app.Activity;
-import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.provider.MediaStore.Images.Media;
 import android.widget.Toast;
-
-import java.io.FileNotFoundException;
-
-
+import android.provider.MediaStore.Images.Media;
+import android.content.ContentValues;
 public class MainActivity extends Activity {
-
     final static int CAMERA_RESULT = 0;
     Uri imageFileUri;
+    // User interface elements, specified in res/layout/main.xml
     ImageView returnedImageView;
     Button takePictureButton;
     Button saveDataButton;
@@ -32,49 +29,56 @@ public class MainActivity extends Activity {
     EditText titleEditText;
     EditText descriptionEditText;
 
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+        // Set the content view to be what is defined in the res/layout/main.xml file
         setContentView(R.layout.activity_main);
-
+        // Get references to UI elements
         returnedImageView = (ImageView) findViewById(R.id.ReturnedImageView);
         takePictureButton = (Button) findViewById(R.id.TakePictureButton);
         saveDataButton = (Button) findViewById(R.id.SaveDataButton);
-        titleTextView = (TextView) findViewById(R.id.DescriptionTextView);
+        titleTextView = (TextView) findViewById(R.id.TitleTextView);
+        descriptionTextView = (TextView) findViewById(R.id.DescriptionTextView);
         titleEditText = (EditText) findViewById(R.id.TitleEditText);
         descriptionEditText = (EditText) findViewById(R.id.DescriptionEditText);
-
+// Set all except takePictureButton to not be visible initially
+// View.GONE is invisible and doesn't take up space in the layout
         returnedImageView.setVisibility(View.GONE);
         saveDataButton.setVisibility(View.GONE);
         titleTextView.setVisibility(View.GONE);
         descriptionTextView.setVisibility(View.GONE);
         titleEditText.setVisibility(View.GONE);
         descriptionEditText.setVisibility(View.GONE);
-
-        takePictureButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        // When the Take Picture Button is clicked
+        takePictureButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v)
+            {
+                // Add a new record without the bitmap
+                // returns the URI of the new record
                 imageFileUri = getContentResolver().insert(Media.EXTERNAL_CONTENT_URI, new ContentValues());
-                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                i.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
+                // Start the Camera App
+                Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                i.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageFileUri);
                 startActivityForResult(i, CAMERA_RESULT);
             }
         });
 
-        saveDataButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        saveDataButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v)
+            {
+                // Update the MediaStore record with Title and Description
                 ContentValues contentValues = new ContentValues(3);
                 contentValues.put(Media.DISPLAY_NAME, titleEditText.getText().toString());
                 contentValues.put(Media.DESCRIPTION, descriptionEditText.getText().toString());
-                getContentResolver().update(imageFileUri, contentValues, null, null);
-
-                Toast bread = Toast.makeText(MainActivity.this, "Recod Updated", Toast.LENGTH_SHORT);
-                bread.show();
-
+                getContentResolver().update(imageFileUri,contentValues,null,null);
+                // Tell the user
+                Toast bread = Toast.makeText(MainActivity.this, "Record Updated", Toast.LENGTH_SHORT);
+                        bread.show();
+                // Go back to the initial state, set Take Picture Button Visible
+                // hide other UI elements
                 takePictureButton.setVisibility(View.VISIBLE);
-
                 returnedImageView.setVisibility(View.GONE);
                 saveDataButton.setVisibility(View.GONE);
                 titleTextView.setVisibility(View.GONE);
@@ -84,10 +88,11 @@ public class MainActivity extends Activity {
             }
         });
     }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
         super.onActivityResult(requestCode, resultCode, intent);
-        if (resultCode == RESULT_OK) {
+        if (resultCode == RESULT_OK)
+        {
             // The Camera App has returned
             // Hide the Take Picture Button
             takePictureButton.setVisibility(View.GONE);
@@ -101,36 +106,41 @@ public class MainActivity extends Activity {
             // Scale the image
             int dw = 200; // Make it at most 200 pixels wide
             int dh = 200; // Make it at most 200 pixels tall
-            try {
+            try
+            {
                 // Load up the image's dimensions not the image itself
                 BitmapFactory.Options bmpFactoryOptions = new BitmapFactory.Options();
                 bmpFactoryOptions.inJustDecodeBounds = true;
                 Bitmap bmp = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageFileUri), null, bmpFactoryOptions);
-                int heightRatio = (int) Math.ceil(bmpFactoryOptions.outHeight / (float) dh);
-                int widthRatio = (int) Math.ceil(bmpFactoryOptions.outWidth / (float) dw);
-                Log.v("HEIGHTRATIO", "" + heightRatio);
-                Log.v("WIDTHRATIO", "" + widthRatio);
+                int heightRatio = (int)Math.ceil(bmpFactoryOptions.outHeight/(float)dh);
+                int widthRatio = (int)Math.ceil(bmpFactoryOptions.outWidth/(float)dw);
+                Log.v("HEIGHTRATIO",""+heightRatio);
+                Log.v("WIDTHRATIO",""+widthRatio);
                 // If both of the ratios are greater than 1,
                 // one of the sides of the image is greater than the screen
-                if (heightRatio > 1 && widthRatio > 1) {
-                    if (heightRatio > widthRatio) {
+                if (heightRatio > 1 && widthRatio > 1)
+                {
+                    if (heightRatio > widthRatio)
+                    {
                         // Height ratio is larger, scale according to it
                         bmpFactoryOptions.inSampleSize = heightRatio;
-                    } else {
+                    }
+                    else
+                    {
                         // Width ratio is larger, scale according to it
                         bmpFactoryOptions.inSampleSize = widthRatio;
                     }
 
-
                 }
-
                 // Decode it for real
                 bmpFactoryOptions.inJustDecodeBounds = false;
-                bmp = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageFileUri), null, bmpFactoryOptions);
+                bmp = BitmapFactory.decodeStream(getContentResolver(). openInputStream(imageFileUri), null, bmpFactoryOptions);
                 // Display it
                 returnedImageView.setImageBitmap(bmp);
-            } catch (FileNotFoundException e) {
-                Log.v("ERROR", e.toString());
+            }
+            catch (FileNotFoundException e)
+            {
+                Log.v("ERROR",e.toString());
             }
         }
     }
