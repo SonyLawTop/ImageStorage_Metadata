e586673f4e2b88cec5bc695ea50cef30d5f0a8bb
SonyLawTop <topsonylaw@gmail.com>
2015-05-19 18:40:09 +0700

    commit

--- ImageStorage_Metadata/app/src/main/java/com/sonylawtop/root/imagestorage_metadata/MainActivity.java
+++ ImageStorage_Metadata/app/src/main/java/com/sonylawtop/root/imagestorage_metadata/MainActivity.java
@@ -1,12 +1,26 @@
 package com.sonylawtop.root.imagestorage_metadata;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    final static int CAMERA_RESULT =0;
+    Uri imageFileUri;
+    ImageView returnedImageView;
+    Button takePictureButton;
+    Button saveDataButton;
+    TextView titleTextView;
+    TextView descriptionTextView;
+    EditText titleEditText;
+    EditText descriptionEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,25 +29,5 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
